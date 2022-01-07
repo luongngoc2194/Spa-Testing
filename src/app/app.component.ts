c6ab21be9b40416379288c8da31af578139fd36d
Your Name <you@example.com>
2022-01-08 02:06:56 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, VERSION} from '@angular/core';
+import {AccountService} from './core/services/account.service';
+import {Observable, Subject} from 'rxjs';
+import {Account, createAccount, createParamSearch, ParamSearch} from './core/model/account.model';
+import {takeUntil} from 'rxjs/operators';
+import {Accounts} from './core/data/account';
+import * as faker from 'faker';
+import {FormAccountService} from './core/services/form-account.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'userlist-angular';
+export class AppComponent implements OnInit {
+  name = 'Angular ' + VERSION.major;
+  account: Account[] = [];
+  unSubscribeAll: Subject<any>;
+  alLength: any;
+  page = 1;
+  perPage = 10;
+
+  searchFormData!: FormGroup;
+  param!: ParamSearch;
+
+  constructor(private accountService: AccountService, public formAccountSV: FormAccountService, private fb: FormBuilder) {
+    // read data from file to localstorage
+    this.unSubscribeAll = new Subject<any>();
+    this.loadDataToLocal();
+  }
+
+  ngOnInit(): void {
+    // this.getAllAccount();
+    this.searchFormData = this.fb.group({
+      start: [''],
+      limit: [''],
+      last_name: [''],
+      first_name: [''],
+      email: [''],
+      address: [''],
+      gender: ['']
+    });
+  }
+
+  loadDataToLocal(): void {
+    localStorage.setItem('accounts', JSON.stringify(Accounts));
+  }
+
+
+  searchForm() {
+
+    this.param = {
+      start: this.searchFormData.value.state || 0,
+      limit: this.searchFormData.value.limit || 600,
+      last_name: this.searchFormData.value.last_name,
+      first_name: this.searchFormData.value.first_name,
+      email: this.searchFormData.value.email,
+      address: this.searchFormData.value.address,
+      gender: this.searchFormData.value.gender,
+    };
+    this.formAccountSV.formSearch$.next(this.param);
+    console.log(this.param);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {fakeBackendProvider} from './core/services/fake-backend';
+import {AccountService} from './core/services/account.service';
+import {HttpClientModule} from '@angular/common/http';
+import { ListAccountComponent } from './core/components/list-account/list-account.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
+import { FormAccountComponent } from './core/components/form-account/form-account.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatOptionModule} from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, HttpClientModule, NgxPaginationModule, BrowserAnimationsModule, MaterialModule, ReactiveFormsModule, MatSelectModule, MatOptionModule],
+  declarations: [AppComponent, ListAccountComponent, FormAccountComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    // provider used to create fake backend,
+    AccountService,
+    fakeBackendProvider
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
